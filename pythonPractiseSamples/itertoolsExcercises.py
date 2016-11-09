8ce37073b80933b5317152e173723c1a2377fd12
Damian Ziobro <damian@xmementoit.com>
2016-11-09 20:53:56 +0000

    python: itertools: change functions to be tested using real Unit Tests from Python's unittest module

--- pythonPractiseSamples/itertoolsExcercises.py
+++ pythonPractiseSamples/itertoolsExcercises.py
@@ -5,22 +5,26 @@
 # Copyright © 2016 Damian Ziobro <damian@xmementoit.com>
 
 import itertools
+import unittest
 
-print ("--------------------------------------------------------------------------------")
-array = [1,2,3,4,5]
-print ("accumulate:")
-for element in itertools.accumulate(array):
-    print(element)
+class TestItertoolsMethods(unittest.TestCase):
 
-array1 = [1,2,3]
-array2 = [4,5,6]
-print ("--------------------------------------------------------------------------------")
-print ("chain:")
-for element in itertools.chain(array1, array2):
-    print (element)
+    def setUp(self):
+        self.array = [1,2,3,4,5,6,7,8,9]
+        self.array1 = [1,2,3]
+        self.array2 = [4,5,6]
 
-print ("--------------------------------------------------------------------------------")
-print ("islice:")
-for element in itertools.islice(array, 2, 4):
-    print (element)
-    
+    def test_accumulate(self):
+        self.assertEqual(tuple(itertools.accumulate(self.array)), (1,3,6,10,15,21,28,36,45))
+
+    def test_chain(self):
+        self.assertEqual(tuple(itertools.chain(self.array1, self.array2)), (1,2,3,4,5,6))
+
+    def test_islice(self):
+        self.assertEqual(tuple(itertools.islice(self.array, 2,4)), (3,4)) #as we number from 0
+        self.assertEqual(len(tuple(itertools.islice(self.array, 2,7))), 5)
+        
+
+if __name__ == "__main__": 
+    print ("running unittests for itertools")
+    unittest.main();

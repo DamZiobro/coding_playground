1f25eeac471ba2d7869dec346d18a02e789a8ba5
Damian Ziobro <damian@xmementoit.com>
2016-11-17 08:24:21 +0000

    reExcercises.py: prepare file

---
+++ pythonPractiseSamples/reExcercises.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Damian Ziobro <damian@xmementoit.com>
+
+import unittest
+import re
+
+class TestRetoolsMethods(unittest.TestCase):
+
+    def setUp(self):
+        pattern      = re.compile("d")
+
+if __name__ == "__main__": 
+    print ("running unittests for regular expressions")
+    unittest.main();

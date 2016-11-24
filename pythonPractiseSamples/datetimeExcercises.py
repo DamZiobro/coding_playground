950cff1590da35215cb38a9e05bf5a4cd2cdd18f
Damian Ziobro <damian@xmementoit.com>
2016-11-24 08:18:18 +0000

    datetimeExcercises: test datetime.time()

--- pythonPractiseSamples/datetimeExcercises.py
+++ pythonPractiseSamples/datetimeExcercises.py
@@ -11,9 +11,13 @@ class TestDatetimeMethods(unittest.TestCase):
 
     def setUp(self):
         self.sampleDate      = datetime.date(2016,11,23)
+        self.sampleTime      = datetime.time(10,30,50)
 
     def test_dateConstructor(self):
         self.assertEqual(str(self.sampleDate), "2016-11-23")
+
+    def test_timeConstructor(self):
+        self.assertEqual(str(self.sampleTime), "10:30:50")
 
 
 if __name__ == "__main__": 

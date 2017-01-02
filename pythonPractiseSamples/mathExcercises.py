a0d8eff20cfd8b60be005e31692af74837ca16f5
Damian Ziobro <damian@xmementoit.com>
2017-01-02 09:26:25 +0000

    mathExcercises: test math.ceil() function

---
+++ pythonPractiseSamples/mathExcercises.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Damian Ziobro <damian@xmementoit.com>
+
+import unittest
+import math
+
+class TestMathMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.number = 3.5
+
+    def test_ceil(self):
+        self.assertEqual(math.ceil(self.number), 4);
+
+if __name__ == "__main__": 
+    print ("running unittests for math")
+    unittest.main();

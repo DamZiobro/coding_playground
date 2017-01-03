3acd07433f7835da251f6990659482663686a8d1
Damian Ziobro <damian@xmementoit.com>
2017-01-03 09:45:44 +0000

    mathExcercises: test math.fabs()

--- pythonPractiseSamples/mathExcercises.py
+++ pythonPractiseSamples/mathExcercises.py
@@ -11,9 +11,13 @@ class TestMathMethods(unittest.TestCase):
 
     def setUp(self):
         self.number = 3.5
+        self.negativeNumber = -3.5
 
     def test_ceil(self):
         self.assertEqual(math.ceil(self.number), 4);
+
+    def test_fabs(self):
+        self.assertEqual(math.fabs(self.negativeNumber), 3.5);
 
 if __name__ == "__main__": 
     print ("running unittests for math")

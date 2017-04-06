6a29e9f963af4920b21c64d157ca90b0d7d081c4
Damian Ziobro <damian@xmementoit.com>
2017-04-06 16:30:50 +0100

    pythonPractices: implement parallelForLoop.py

---
+++ pythonPractiseSamples/parallelForLoop.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Damian Ziobro <damian@xmementoit.com>
+#
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+n=1000000
+
+def squareRoot(i):
+    return i*i
+
+cpus = multiprocessing.cpu_count()
+#cpus = 1
+
+results = Parallel(n_jobs=cpus)(delayed(squareRoot)(i) for i in range(n))
+#print results

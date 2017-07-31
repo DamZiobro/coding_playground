f77069172b35fda981f8e03780fd9b0c28b73e1c
Damian Ziobro <damian@xmementoit.com>
2017-07-31 18:10:00 +0100

    javaSpringSamples: springGradleInitDestoryMethod - add sample of usage init-method and destroy-method parameters for Spring Beans

---
+++ javaspringSamples/springGradleInitDestroyMethods/src/main/java/hello/Greeter.java
@@ -0,0 +1,37 @@
+/*
+ * Greeter.java
+ * Copyright (C) 2017 damian <damian@damian-laptop>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class Greeter
+{
+  String message;
+  String secondMessage;
+  public Greeter(String message) {
+    this.message = message;
+  }
+
+  public void setSecondMessage(String secondMessage) {
+    this.secondMessage = secondMessage; 
+  }
+
+  public String sayHello(){
+    return message; 
+  }
+  
+  public String saySecondMessage(){
+    return secondMessage; 
+  }
+  
+  public void init(){
+    System.out.println("Initializing Bean Greeter");
+  }
+
+  public void destroy(){
+    System.out.println("Destroying Bean Greeter");
+  }
+}
+
+

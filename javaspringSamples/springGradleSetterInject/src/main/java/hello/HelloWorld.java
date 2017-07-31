6949d42cc85f4106aa16eb8d28461f3c1658604b
Damian Ziobro <damian@xmementoit.com>
2017-07-31 17:30:00 +0100

    javaSpringSamples: add springGradleSetterInject sample - injecting java Spring dependencies using setter method

---
+++ javaspringSamples/springGradleSetterInject/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * HelloWorld.java
+ * Copyright (C) 2017 damian <damian@damian-laptop>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+      Greeter greeter = (Greeter) ctx.getBean("greeter");
+      System.out.println(greeter.sayHello());
+      System.out.println(greeter.saySecondMessage());
+    }
+}
+
+

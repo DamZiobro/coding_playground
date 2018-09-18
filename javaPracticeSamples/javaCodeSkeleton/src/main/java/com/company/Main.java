3b746b150e7977e6e3cf608b365e0514c0c3c770
Damian Ziobro <damian@xmementoit.com>
2018-09-18 19:31:32 +0100

    javaPracticeSamples: add Java skeleton app

---
+++ javaPracticeSamples/javaCodeSkeleton/src/main/java/com/company/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Main.java
+ */
+
+package com.company;
+
+import com.company.Calculator;
+
+public class Main
+{
+  public static void main (String[] args) {
+    Calculator calc = new Calculator();
+
+    System.out.println("add(2,3): " + calc.add(2,3));
+  }
+
+}
+
+

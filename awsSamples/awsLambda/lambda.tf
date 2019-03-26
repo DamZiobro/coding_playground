cf07e1f6c2c3030f73ead6a1516b9df90cc45aaa
Damian Ziobro <damian.ziobro1@nhs.net>
2019-03-26 15:27:25 +0000

    awsSources: add API Gateway integration

--- awsSamples/awsLambda/lambda.tf
+++ awsSamples/awsLambda/lambda.tf
@@ -39,3 +39,15 @@ resource "aws_iam_role" "lambda_exec" {
 }
 EOF
 }
+
+# add access to the API Gateway resources
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
+}

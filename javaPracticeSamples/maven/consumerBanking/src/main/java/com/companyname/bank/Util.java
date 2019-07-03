457b29ddb5d951db84f0eb073e38a413f1178a4e
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-07-03 11:41:08 +0100

    add Maven samples

---
+++ javaPracticeSamples/maven/consumerBanking/src/main/java/com/companyname/bank/Util.java
@@ -0,0 +1,13 @@
+/*
+ * Util.java
+ * Copyright (C) 2019 damian <damian@damian-laptop>
+ *
+ * Distributed under terms of the MIT license.
+ */
+package com.companyname.bank;
+
+public class Util {
+   public static void printMessage(String message){
+      System.out.println(message);
+   }
+}

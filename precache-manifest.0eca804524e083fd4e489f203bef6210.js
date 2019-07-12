7b57d122043e74d617ddc78a8a86d96c034f68da
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-07-12 10:12:36 +0100

    Updates

---
+++ precache-manifest.0eca804524e083fd4e489f203bef6210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd36cfb616610517ae1f2cb6415cae9d",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "32e647659e7ac4a09c06",
+    "url": "/react-tutorial/static/css/main.f02851ef.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "32e647659e7ac4a09c06",
+    "url": "/react-tutorial/static/js/main.5065fb6b.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);

6c742e170616f8d203b7de92c1ce9b0386d1d70a
Damian Ziobro <damian@xmementoit.com>
2020-01-14 16:36:42 +0000

    pythonFlaskAWS: add Redis

--- python/pythonFlaskAWS/app.py
+++ python/pythonFlaskAWS/app.py
@@ -5,17 +5,29 @@
 # Copyright © 2020 damian <damian@damian-desktop>
 #
 # Distributed under terms of the MIT license.
-
+import time  
+import redis
 from flask import Flask
-import os
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
-@app.route("/")
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('reaches')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(1)
+
+@app.route('/')
 def hello():
-    return "Hello Flask World!!"
-
+    count = get_hit_count()
+    return 'Hello Flask World! I have been reached {} times.\n'.format(count)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(host="0.0.0.0", debug=True)
+

1900a24a4b5ad35d6e2a1fc1d1346f2dcc9d2c87
Damian Ziobro <damian@xmementoit.com>
2020-01-29 09:20:20 +0000

    python: add f-strings

---
+++ python/fstrings.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":    
+
+    var1 = "VAR1_VALUE"
+
+    #using format
+    print("This is {}".format(var1))
+    #using f-string
+    print(f"This is {var1}")
+
+#OUTPUT:
+#This is VAR1_VALUE
+#This is VAR1_VALUE

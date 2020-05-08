a22190f9f0b2068df25c11c4ccfa1cf5992ee375
Damian Ziobro <damian@xmementoit.com>
2020-05-08 10:58:31 +0100

    aws: update aws_get_log_events.py

--- aws/cloudwatch/aws_get_log_events.py
+++ aws/cloudwatch/aws_get_log_events.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8
 """Print log event messages from a CloudWatch log group.
 
-Usage: aws_get_log_events.py <LOG_GROUP_NAME> [--log-stream=<LOG_STREAM_NAME>] [--start=<START>] [--end=<END>] [--limit=<LIMIT>] [--filter=<FILTER_PATTERN>] [--regex=<REGEX_PATTERN>] [--log-streams-number=<LOG_STREAMS_NUMBER>]
+Usage: aws_get_log_events.py <LOG_GROUP_NAME> [--log-stream=<LOG_STREAM_NAME>] [--start=<START>] [--end=<END>] [--limit=<LIMIT>] [--filter=<FILTER_PATTERN>] [--regex=<REGEX_PATTERN>] [--log-streams-number=<LOG_STREAMS_NUMBER>] [--is_print=IS_PRINT]
        aws_get_log_events.py -h --help
 
 Options:
@@ -14,6 +14,7 @@ Options:
   --regex=<FILTER_PATTERN> Patter to regex messages
   --filter=<FILTER_PATTERN> Patter to filter messages
   --log-streams-number=<LOG_STREAMS_NUMBER> Number of recent logs streams to process
+  --is_print=<IS_PRINT> Should logs be printed to the console
   -h --help           Show this screen.
 
 """
@@ -24,14 +25,72 @@ import maya
 import re
 import sys
 import datetime
+import concurrent.futures
+import logging 
+import os
 
-#def get_log_events(log_group, log_stream=None, start_time=None, end_time=None, limit=10000, filter="", log_streams_number=1, regex=""):
+client = boto3.client('logs')
+logging.basicConfig(level=logging.INFO)
+
+def ensure_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def get_logs(log_group, stream, start_time=None, end_time=None, limit=10000, filter="", regex="", is_print="no"):
+
+    LOG_DIR="/tmp/awslogs"
+
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+
+    if isinstance(stream, str):
+        log_stream_name = stream
+        filename = f"{LOG_DIR}/" + log_stream_name.split("/")[-1]
+        logging.info(f"GETTING log stream name: {log_stream_name}; filename: {filename}")
+    else:
+        log_stream_name = stream.get("logStreamName")
+        creation_time = stream.get("creationTime")
+        creation_time = datetime.datetime.fromtimestamp(int(creation_time)/1000)
+        last_event_time = stream.get("lastEventTimestamp")
+        last_event_time = datetime.datetime.fromtimestamp(int(last_event_time)/1000)
+        filename = f"{LOG_DIR}/" + log_stream_name.split("/")[-1]
+        logging.info(f"GETTING log stream name: {log_stream_name}; filename: {filename}; creation_time: {creation_time}; last_event_time: {last_event_time}")
+
+    kwargs = {
+        'logGroupName': log_group,
+        'logStreamNames': [log_stream_name],
+        'limit': limit,
+    }
+
+    if start_time is not None:
+        kwargs['startTime'] = start_time
+    if end_time is not None:
+        kwargs['endTime'] = end_time
+    if filter != "":
+        kwargs['filterPattern'] = filter
+
+    file = open(filename, "w")
+    while True:
+        resp = client.filter_log_events(**kwargs)
+        #yield from resp['events']
+        for event in resp['events']:
+            log_message = event['message'].rstrip()
+            if re.search(regex, log_message):
+                if(is_print.startswith("y")):
+                    print(f"{log_stream_name} => {log_message}")
+                file.write(log_message + "\n")
+
+        try:
+            kwargs['nextToken'] = resp['nextToken']
+        except KeyError:
+            file.close()
+            break
+
+    return log_stream_name
 
 
 
-def get_log_events(log_group, log_stream=None, start_time=None, end_time=None, limit=10000, filter="", log_streams_number=1, regex=""):
-
-    client = boto3.client('logs')
+def get_log_events(log_group, log_stream=None, start_time=None, end_time=None, limit=10000, filter="", log_streams_number=1, regex="", is_print="no"):
 
     log_streams = [log_stream]
     if start_time is None and end_time is None and log_stream is None:
@@ -39,55 +98,21 @@ def get_log_events(log_group, log_stream=None, start_time=None, end_time=None, l
             logGroupName=log_group, 
             orderBy="LastEventTime",
             descending=True,
+            limit=log_streams_number
         )
 
         log_streams = response.get("logStreams")
 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        try:
+            stream_futures = {executor.submit(get_logs, log_group, stream, start_time, end_time, limit, filter, regex, is_print): stream for stream in log_streams}
+        except Exception as ex:
+            logging.error(f"exception: {ex}")
 
-    for i in range(0, log_streams_number):
-        stream = log_streams[i]
-        filename = "/tmp/logs"
-        if isinstance(stream, str):
-            log_stream_name = stream
-            filename = "/tmp/" + log_stream_name.split("/")[-1]
-            print(f"GETTING log stream name: {log_stream_name}; filename: {filename}")
-        else:
-            log_stream_name = stream.get("logStreamName")
-            creation_time = stream.get("creationTime")
-            creation_time = datetime.datetime.fromtimestamp(int(creation_time)/1000)
-            last_event_time = stream.get("lastEventTimestamp")
-            last_event_time = datetime.datetime.fromtimestamp(int(last_event_time)/1000)
-            filename = "/tmp/" + log_stream_name.split("/")[-1]
-            print(f"GETTING log stream name: {log_stream_name}; filename: {filename}; creation_time: {creation_time}; last_event_time: {last_event_time}")
-
-        kwargs = {
-            'logGroupName': log_group,
-            'logStreamNames': [log_stream_name],
-            'limit': limit,
-        }
-
-        if start_time is not None:
-            kwargs['startTime'] = start_time
-        if end_time is not None:
-            kwargs['endTime'] = end_time
-        if filter != "":
-            kwargs['filterPattern'] = filter
-
-        file = open(filename, "w")
-        while True:
-            resp = client.filter_log_events(**kwargs)
-            #yield from resp['events']
-            for event in resp['events']:
-                log_message = event['message'].rstrip()
-                if re.search(regex, log_message):
-                    #print(log_message)
-                    file.write(log_message + "\n")
-
-            try:
-                kwargs['nextToken'] = resp['nextToken']
-            except KeyError:
-                file.close()
-                break
+        for future in concurrent.futures.as_completed(stream_futures):
+            stream = stream_futures[future]
+            resp = future.result()
+            logging.info(f"finished getting logs from stream: {resp}")
 
 
 def milliseconds_since_epoch(time_string):
@@ -108,6 +133,7 @@ if __name__ == '__main__':
     filter=""
     regex=""
     log_streams_number = 1
+    is_print="no"
 
     if args['--log-stream']:
         log_stream = args['--log-stream']
@@ -123,6 +149,11 @@ if __name__ == '__main__':
 
     if args['--regex']:
         regex = args['--regex']
+
+    if args['--is_print']:
+        is_print = args['--is_print']
+        if is_print.startswith("y"):
+            logging.getLogger().setLevel(logging.WARN)
 
     if args['--start']:
         try:
@@ -145,4 +176,5 @@ if __name__ == '__main__':
         filter=filter,
         log_streams_number=log_streams_number,
         regex=regex,
+        is_print=is_print,
     )

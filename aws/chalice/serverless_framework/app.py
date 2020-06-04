ddeeabecd3c488ff083058fa5125c8335d3a02c6
Damian Ziobro <damian.ziobro@morrisonsplc.co.uk>
2020-06-04 21:36:55 +0100

    aws: chalice: update chalice hello world app

--- aws/chalice/serverless_framework/app.py
+++ aws/chalice/serverless_framework/app.py
@@ -17,23 +17,3 @@ app.experimental_feature_flags.update([
 app.register_blueprint(lambda_function_1_blueprint)
 app.register_blueprint(lambda_function_2_blueprint)
 app.register_blueprint(lambda_function_3_blueprint)
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-#@app.route('/hello/{name}')
-#def hello_name(name):
-    ## '/hello/james' -> {"hello": "james"}
-    #return {'hello': name}
-
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#

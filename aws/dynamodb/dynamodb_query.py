17b6c0f7d421e297829c37f9c5e0ce2e860cf033
Damian Ziobro <damian.ziobro@morrisonsplc.co.uk>
2020-12-11 11:16:26 +0000

    aws: dynamodb_query => implement dynamodb script and make it generic

--- aws/dynamodb/dynamodb_query.py
+++ aws/dynamodb/dynamodb_query.py
@@ -1,72 +1,29 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2020 damian <damian@damian-laptop>
-#
-# Distributed under terms of the MIT license.
 
+import fire
 import boto3
-import datetime
+import logging
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
-table = dynamodb.Table('dev.xxwmm.storepick.items_v2')
+"""
+Example usage:
+    python dynamodb_query.py --table_name="dev.orders" --query_key="orderDate" --query_value="10122020"
+"""
 
-batch_id = "16"
+def query_dynamodb(table_name, query_key, query_value):
+    print(f"Querying dynamodb table: {table_name} with filter => {query_key}={query_value}")
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
 
-start_time = datetime.datetime.now()
-print("Start index-based DB query...")
-resp = table.query(
-    IndexName = 'batchId-index',
-    KeyConditionExpression=Key('batchId').eq(batch_id)
-)
-end_time = datetime.datetime.now()
-
-delta = end_time - start_time
-print(f"index-based query took: {delta}")
-print(f"got {len(resp['Items'])} items for batch_id {batch_id}")
-
-start_time = datetime.datetime.now()
-print("Start non-index-based DB queries...")
-
-items_list = []
-NUMBER_ITEMS_PARTITION = 50
-for index in range(0, NUMBER_ITEMS_PARTITION + 1):
-    tmp_resp = table.query(
-        KeyConditionExpression=Key('outOfStock').eq(f'outOfStock_{batch_id}_{index}'),
-        ConsistentRead=True
+    response = table.query(
+        KeyConditionExpression=Key(str(query_key)).eq(str(query_value))
     )
-    items_list += tmp_resp['Items']
 
-resp = {}
-resp['Items'] = items_list
+    for item in response['Items']:
+        print(str(item))
 
-# THE BELOW QUERY THROWS EXCEPTION AS ONLY `eq` is allowed for KeyConditionExpression here...
-#resp = table.query(
-    #KeyConditionExpression=Key('outOfStock').begins_with('outOfStock_{batch_id}'),
-    #ConsistentRead=True
-#)
-end_time = datetime.datetime.now()
+    items_number = len(response['Items'])
+    print(f"number of items for query '{query_key}={query_value}': {items_number}")
 
-delta = end_time - start_time
-
-print(f"no-index-based query took: {delta}")
-print(f"got {len(resp['Items'])} items for batch_id {batch_id}")
-
-#print("The query returned the following items:")
-#for item in resp['Items']:
-    #print(item)
-
-#config_table = dynamodb.Table('dev.xxwmm.storepick.config')
-
-#response_array = []
-#for status in ['NEW', 'PENDING', 'ITEMS_REVIEW', 'ITEMS_UPLOADED', 'IN_PROGRESS', 'CANCELLED', 'COMPLETED']:
-    #table_items = config_table.query(
-        #IndexName = 'status-index',
-        #KeyConditionExpression = Key('status').eq(status),
-    #)
-    #response_array = response_array + (table_items['Items'])
-
-#print(len(response_array))
-
+if __name__ == "__main__":
+    fire.Fire(query_dynamodb)

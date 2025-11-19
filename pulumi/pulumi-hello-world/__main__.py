2425b58cf5e0ffe9cb09085bda40953e8a0b3862
Damian Ziobro <damian@xmementoit.com>
2025-11-19 20:10:45 +0000

    pulumi: add pulumi-hello-world

---
+++ pulumi/pulumi-hello-world/__main__.py
@@ -0,0 +1,11 @@
+import pulumi
+from pulumi_command import local
+
+pulumi.log.info("Deploying command 'hello' - START")
+hello = local.Command(
+    "hello",
+    create="echo 'Hello from Pulumi local!'"
+)
+
+pulumi.log.info("Deploying command 'hello' - END")
+pulumi.export("message", hello.stdout)
